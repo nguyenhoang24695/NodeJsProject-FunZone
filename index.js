d9de687e5401963dd8d242fe1875bb4338da4889
Hipocrady <napa.nguyenhoang@gmail.com>
2018-02-06 23:33:42 +0700

    fixing gate

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var mongooseTransactions = require('mongoose-transaction');
+var mongooseTransactions = require('mongoose-transactions');
 var cors = require('cors');
 var app = express();
 const fileUpload = require('express-fileupload');
 // Kết nối đến mongodb ở project tạm
 mongoose.connect('mongodb://nguyenhoang95:liuyifei1A@ds123658.mlab.com:23658/fun_zone_project');
 mongoose.Promise = global.Promise;
-
+app.set('port', (process.env.PORT || 3000));
 
 app.use(cors());
 app.use(fileUpload());
@@ -20,6 +20,6 @@ app.use(express.static('./public'));
 
 var applicationRoutes = require('./routes/applicationRoutes');
 applicationRoutes(app);
-app.listen(3000, function(){
+app.listen(app.get('port'), function(){
 	console.log('I am running at port 3000!');
 });
